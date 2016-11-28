8204941c3c7d1f51a569ca3ed485dc673c84b652
ciaran <ciaranroche@hotmail.co.uk>
2016-11-28 20:50:44 +0000

    Movie class added

---
+++ src/models/Movie.java
@@ -0,0 +1,38 @@
+package models;
+
+import java.util.Objects;
+
+public class Movie {
+	static Long	counter = (long) 01;
+	
+	public String title;
+	public String year;
+	public String url;
+
+	private Long id;
+	
+	public Movie(String title, String year, String url){
+		this.id = counter ++;
+		this.title = title;
+		this.year = year;
+		this.url = url;
+	}
+	
+	@Override
+	public String toString(){
+		return "Title: " +title+ " Year: " +year+ " Url: " + url;
+	}
+	
+	@Override
+	public boolean equals(final Object obj){
+		if (obj instanceof Movie){
+			final Movie other = (Movie) obj;
+			return Objects.equals(title, other.title)
+					&& Objects.equals(year, other.year)
+					&& Objects.equals(url, other.url);
+		}else{
+			return false;
+		}
+	}
+
+}
