6f71510b2202573883fddaaf7a47ae366501d04e
ciaran <ciaranroche@hotmail.co.uk>
2016-11-30 09:35:53 +0000

    Data Added, Serializer and XMLSerializer is added to utils

---
+++ src/utils/XMLSerializer.java
@@ -0,0 +1,64 @@
+package utils;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import java.util.Stack;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class XMLSerializer implements Serializer {
+	
+	private Stack stack = new Stack();
+	private File file;
+	
+	public XMLSerializer(File file){
+		this.file = file;
+	}
+
+	@Override
+	public void push(Object o){
+		stack.push(o);
+
+	}
+
+	@Override
+	public Object pop() {
+		return stack.pop();
+	}
+
+	@Override
+	public void write() throws Exception {
+		ObjectOutputStream os = null;
+		
+		try{
+			XStream xstream = new XStream(new DomDriver());
+			os = xstream.createObjectOutputStream(new FileWriter(file));
+			os.writeObject(stack);
+		}finally{
+			if(os!=null){
+				os.close();
+			}
+		}
+	}
+
+	@Override
+	public void read() throws Exception {
+		ObjectInputStream is = null;
+		
+		try{
+			XStream xstream = new XStream(new DomDriver());
+		    is = xstream.createObjectInputStream(new FileReader(file));
+		    stack = (Stack) is.readObject();
+		}finally{
+			if(is!=null){
+				is.close();
+			}
+		}
+	}
+
+}
